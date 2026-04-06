3244b9cf99b6e17ecb08601f51ef7c7bd608d47d
vimukthi7777 <svimukthi7777@gmail.com>
2026-04-06 10:19:30 +0530

    Initial commit

---
+++ src/test/java/com/vimukthi/studentsystem/StudentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vimukthi.studentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
